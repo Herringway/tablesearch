a7639640d24b049bfb35a6e4b19d05ae9721d5e5
Cameron Ross <elpenguino@gmail.com>
2019-04-30 01:46:14 -0300

    rewrite to remove old std.stream dependency and boost efficiency

--- source/app.d
+++ source/app.d
@@ -1,89 +1,127 @@
-module main;
+module app;
 
-import std.stdio;
-import std.stream;
-import std.conv;
-import std.getopt;
-import std.format;
 import std.algorithm;
-import std.array;
+import std.conv;
+import std.exception;
+import std.file;
+import std.format;
+import std.getopt;
+import std.meta;
+import std.stdio;
 
 void printHelp(string progname) {
 		stderr.writefln("Usage: %s file val1 val2 ...", progname);
 }
-void printProgressBar(ulong divisor, ulong dividend) {
-	int fill = cast(int)((cast(double)divisor/cast(double)dividend) * 11.0);
-	printProgressBar(fill);
+struct Match {
+	size_t offset;
+	size_t distance;
+	size_t size;
 }
-void printProgressBar(int fill, int length = 10) {
-	char[] outputString = new char[length];
-	outputString[] = ' ';
-	outputString[0..fill] = '=';
-	stdout.write("[",outputString,"]\r");
-	stdout.flush;
-}
+
 int main(string[] argv)
 {
 	if (argv.length < 4) {
 		printHelp(argv[0]);
 		return 1;
 	}
-	ulong maxdist = 200;
+	ulong maxDist = 200;
 	string offsetstr = "0";
-	ulong mindist = 1;
+	ulong minDist = 1;
 	bool unsigned = false;
 	getopt(argv, std.getopt.config.bundling,
 		   "offset|o", &offsetstr,
 		   "unsigned|u", &unsigned,
-		   "maxdist|m", &maxdist,
-		   "mindist|d", &mindist);
+		   "maxdist|m", &maxDist,
+		   "mindist|d", &minDist);
 
 	ulong offset = parseOffset(offsetstr);
-	std.stream.BufferedFile file = new std.stream.BufferedFile(argv[1], FileMode.In, 0x1000000);
-	
-	long[] vals = array(map!((x) => to!long(x))(argv[2..$]));
-	ulong[] vals_unsigned = array(map!((x) => to!ulong(x))(argv[2..$]));
-	ubyte buf;
-	ulong prevoffset;
-	ulong[] matches;
-	int fill = 0;
-	file.seekSet(offset);
-	auto size = file.size;
-	auto readSize = 1;
-	if (unsigned)
-		foreach (val; vals_unsigned)
-			readSize = max(getValMinSizeUnsigned(val), readSize);
-	else
-		foreach (val; vals)
-			readSize = max(getValMinSize(val), readSize);
-	writefln("Using value size %d", readSize);
-	printProgressBar(fill);
-	while (!file.eof) {
-		prevoffset = file.position;
-		if (cast(int)((cast(double)(prevoffset - offset)/cast(double)(size - offset)) * 10.0) > fill) {
-			fill = cast(int)((cast(double)(prevoffset - offset)/cast(double)(size - offset)) * 10.0);
-			printProgressBar(fill);
-		} 
-		if (unsigned) {
-			if (file.readVarValUnsigned(readSize) == vals_unsigned[0]) {
-				foreach (distance; mindist..maxdist)
-					if (readDistanceUnsigned(file, prevoffset, distance, vals.length - 1, readSize) == vals_unsigned[1..$])
-						matches ~= prevoffset;
+	const file = cast(ubyte[])read(argv[1]);
+
+	const searchArrays = buildSearchArrays(argv[2..$].to!(ulong[]), unsigned);
+	writefln!"Searching for: %((%([%-(%02X %)]%|, %))%|, %)"(searchArrays);
+	Match[] matches;
+	while (offset < file.length) {
+		foreach (searchArray; searchArrays) {
+			if (searchArray[0].length+offset >= file.length) {
+				continue;
 			}
-		} else {
-			if (file.readVarVal(readSize) == vals[0]) {
-				foreach (distance; mindist..maxdist)
-					if (        readDistance(file, prevoffset, distance, vals.length - 1, readSize) == vals[1..$])
-						matches ~= prevoffset;
+			if (file[offset..offset+searchArray[0].length] == searchArray[0]) {
+				const startDistance = max(searchArray[0].length, minDist);
+				enforce(startDistance <= maxDist, "Maximum distance smaller than value size");
+				foreach (dist; startDistance..maxDist) {
+					bool matched;
+					foreach (i, byteSequence; searchArray[1..$]) {
+						const newOffset = offset + dist*(i+1);
+						const testNext = file[newOffset..newOffset+byteSequence.length];
+						if (testNext != byteSequence) {
+							matched = false;
+							break;
+						}
+						matched = true;
+					}
+					if (matched) {
+						matches ~= Match(offset, dist, searchArray[0].length);
+					}
+				}
 			}
 		}
-		file.seekSet(prevoffset+readSize);
+		offset++;
 	}
-	foreach (match; matches)
-		writefln("Found match at %d (0x%1$X)", match-1);
+	foreach (match; matches) {
+		writefln!"Found match: 0x%X - %s distance, %s size"(match.offset, match.distance, match.size);
+	}
 	return 0;
 }
-uint getValMinSize(long val) {
+ubyte[][][] buildSearchArrays(ulong[] vals, const bool unsigned) @safe {
+	ubyte[][][] output;
+	ubyte minSize;
+	foreach (val; vals) {
+		ubyte tmpMin;
+		if (unsigned) {
+			tmpMin = getValMinSizeUnsigned(val);
+		} else {
+			tmpMin = getValMinSize(val);
+		}
+		minSize = max(tmpMin, minSize);
+	}
+	union ByteArrayHelper(T) {
+		ubyte[T.sizeof] raw;
+		T value;
+	}
+	static foreach (Type; AliasSeq!(ubyte, ushort, uint, ulong)) {{
+		if (minSize <= Type.sizeof) {
+			ubyte[][] byteArrays;
+			foreach (val_; vals) {
+				ByteArrayHelper!Type val;
+				val.value = cast(Type)val_;
+				byteArrays ~= val.raw.dup;
+			}
+			output ~= byteArrays;
+		}
+	}}
+	return output;
+}
+
+@safe unittest {
+	assert(buildSearchArrays([4], true) ==
+		[
+			[
+				[4]
+			],
+			[
+				[4, 0]
+			],
+			[
+				[4, 0, 0, 0]
+			],
+			[
+				[4, 0, 0, 0, 0, 0, 0, 0]
+			]
+		]
+	);
+}
+
+ubyte getValMinSize(long val) @safe {
 	if (val > 0x7FFFFFFFFFFFFF)
 		return 8;
 	else if (val > 0x7FFFFFFFFFFF)
@@ -100,7 +138,7 @@ uint getValMinSize(long val) {
 		return 2;
 	return 1;
 }
-uint getValMinSizeUnsigned(ulong val) {
+ubyte getValMinSizeUnsigned(ulong val) @safe {
 	if (val > 0xFFFFFFFFFFFFFF)
 		return 8;
 	else if (val > 0xFFFFFFFFFFFF)
@@ -116,37 +154,6 @@ uint getValMinSizeUnsigned(ulong val) {
 	else if (val > 0xFF)
 		return 2;
 	return 1;
-}
-ulong[] readDistanceUnsigned(std.stream.BufferedFile file, ulong offset, ulong distance, uint count, uint readSize) {
-	ulong[] output;
-	foreach (id; 0..count) {
-		file.seekSet(offset + distance * (id+1));
-		output ~= file.readVarValUnsigned(readSize);
-	}
-	return output;
-}
-long[] readDistance(std.stream.BufferedFile file, ulong offset, ulong distance, uint count, uint readSize) {
-	long[] output;
-	foreach (id; 0..count) {
-		file.seekSet(offset + distance * (id+1));
-		output ~= file.readVarVal(readSize);
-	}
-	return output;
-}
-long readVarVal(std.stream.BufferedFile file, uint readSize) {
-	ulong temp = file.readVarValUnsigned(readSize);
-	if ((temp & (1<<(readSize*8-1))) != 0)
-		return cast(long)((cast(long)0x8000000000000000) >> (64-readSize*8-1) | temp);
-	return cast(long)temp;
-}
-ulong readVarValUnsigned(std.stream.BufferedFile file, uint readSize) {
-	ulong temp;
-	ubyte buf;
-	foreach (i; 0..readSize) {
-		file.read(buf);
-		temp |= cast(ulong)buf<<(i*8);
-	}
-	return temp;
 }
 int parseOffset(string arg) {
 	int offset;
